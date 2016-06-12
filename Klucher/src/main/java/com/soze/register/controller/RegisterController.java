53a81819b34116af7bfbc79c22544eae7f3c1112
sozemego <piknypiec@gmail.com>
2016-06-12 21:10:24 +0200

    Logged in user redirection
    If a logged in user goes to /register, they are redirected to /user.

--- Klucher/src/main/java/com/soze/register/controller/RegisterController.java
+++ Klucher/src/main/java/com/soze/register/controller/RegisterController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,7 +40,10 @@ public class RegisterController {
   }
 
   @RequestMapping(method = RequestMethod.GET)
-  public String getRegister() {
+  public String getRegister(Authentication authentication) {
+    if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
+      return "user";
+    }
     return "register";
   }
 
