39ee075ab1c9963d9c19268c156caa4d8758610b
sozemego <piknypiec@gmail.com>
2016-06-15 22:48:25 +0200

    FrontController tests
    Added tests for the front page.

---
+++ Klucher/src/test/java/com/soze/front/controller/FrontControllerTest.java
@@ -0,0 +1,69 @@
+package com.soze.front.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.soze.Klucher;
+import com.soze.TestWithUserBase;
+import com.soze.user.dao.UserDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Klucher.class)
+@WebIntegrationTest
+@Transactional
+@ActiveProfiles("test")
+public class FrontControllerTest extends TestWithUserBase {
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  private MockMvc mvc;
+  
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(springSecurity()).build();
+  }
+  
+  @Test
+  public void getPage() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/")
+        .accept(MediaType.APPLICATION_FORM_URLENCODED)
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("front"));
+  }
+  
+  @Test
+  public void alreadyLoggedInTest() throws Exception {
+    addUserToDbAndLogin("user", "password");
+    mvc.perform(MockMvcRequestBuilders.get("/")
+        .accept(MediaType.APPLICATION_FORM_URLENCODED)
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("dashboard"));
+  }
+}
