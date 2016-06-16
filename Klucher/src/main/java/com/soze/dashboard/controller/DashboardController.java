7e481273ab1f003a2547e51c5430363171a6811c
sozemego <piknypiec@gmail.com>
2016-06-16 19:35:41 +0200

    Dashboard
    Added a DashboardController (/dashboard) and a dashboard.jsp.

---
+++ Klucher/src/main/java/com/soze/dashboard/controller/DashboardController.java
@@ -0,0 +1,31 @@
+package com.soze.dashboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.soze.user.dao.UserDao;
+import com.soze.user.model.User;
+
+@Controller
+public class DashboardController {
+
+  private final UserDao userDao;
+  
+  @Autowired
+  public DashboardController(UserDao userDao) {
+    this.userDao = userDao;
+  }
+  
+  @RequestMapping("/dashboard")
+  public String getDashboard(Authentication authentication, Model model) {
+    String username = authentication.getName();
+    User user = userDao.findOne(username);
+    model.addAttribute("user", user);
+    return "dashboard";
+  }
+  
+  
+}
---
+++ Klucher/src/main/webapp/WEB-INF/jsp/dashboard.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="<c:url value="/resources/css/main.css" />"></link>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="/resources/js/klucher.js"></script>
+</head>
+<body>
+Welcome ${user.username}
+		
+<script>
+
+</script>	
+</body>
+</html>
