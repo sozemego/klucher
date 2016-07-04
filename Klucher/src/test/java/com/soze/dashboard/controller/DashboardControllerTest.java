a4dc1dee87ffa33b2a161ce13b166ab1e76a759e
sozemego <piknypiec@gmail.com>
2016-07-04 20:59:46 +0200

    Simple tests for dashboard

---
+++ Klucher/src/test/java/com/soze/dashboard/controller/DashboardControllerTest.java
@@ -0,0 +1,71 @@
+package com.soze.dashboard.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.soze.Klucher;
+import com.soze.TestWithUserBase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Klucher.class)
+@WebIntegrationTest
+@Transactional
+@ActiveProfiles("test")
+public class DashboardControllerTest extends TestWithUserBase {
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  private MockMvc mvc;
+  
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(springSecurity()).build();
+  }
+  
+  @Test
+  public void testDashboardUnauthorized() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/dashboard")
+        .accept(MediaType.APPLICATION_FORM_URLENCODED)
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+        .andDo(print())
+        .andExpect(status().is3xxRedirection());
+  }
+  
+  @Test
+  public void testDashboard() throws Exception {
+    addUserToDbAndLogin("user", "password");
+    mvc.perform(MockMvcRequestBuilders.get("/dashboard")
+        .accept(MediaType.APPLICATION_FORM_URLENCODED)
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("dashboard"))
+        .andExpect(model().attributeExists("user"))
+        .andExpect(model().attribute("user", hasProperty("username", equalTo("user"))));
+  }
+
+}
