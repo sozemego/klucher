0f7a32b23c5ee7d31839c8b0af3ac5dc0f4f93b5
sozemego <piknypiec@gmail.com>
2016-08-13 10:46:01 +0200

    FileUtils class able to read files from classpath

--- Klucher/src/main/java/com/soze/utils/FileUtils.java
+++ Klucher/src/main/java/com/soze/utils/FileUtils.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 public class FileUtils {
 
   private final ResourceLoader resourceLoader;
-
+  
   @Autowired
   public FileUtils(ResourceLoader resourceLoader) {
     this.resourceLoader = resourceLoader;
@@ -31,13 +31,7 @@ public class FileUtils {
 
   public List<String> readLinesFromFile(String path) throws IOException {
     Resource resource = resourceLoader.getResource("file:" + path);
-    InputStream stream = resource.getInputStream();
-    List<String> lines = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(
-        new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-      lines.addAll(br.lines().collect(Collectors.toList()));
-    }
-    return lines;
+    return readFromResource(resource);
   }
 
   private String concatenateLines(List<String> lines) {
@@ -46,6 +40,21 @@ public class FileUtils {
       sb.append(line);
     }
     return sb.toString();
+  }
+  
+  public List<String> readLinesFromClasspathFile(String path) throws IOException {
+    Resource resource = resourceLoader.getResource("classpath:" + path);
+    return readFromResource(resource);
+  }
+  
+  private List<String> readFromResource(Resource resource) throws IOException {
+    InputStream stream = resource.getInputStream();
+    List<String> lines = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(
+        new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+      lines.addAll(br.lines().collect(Collectors.toList()));
+    }
+    return lines;
   }
 
 }
