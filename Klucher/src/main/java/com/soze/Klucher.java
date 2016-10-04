7248da06ce71d8b54a8b163b93102eda88b3a5f3
sozemego <piknypiec@gmail.com>
2016-10-04 23:34:30 +0200

    Enabled caching (@EnableCaching) and added more logging configuration
    Number of notifications, and list of user ids following or being
    followees are cached. Enabled logging for org.springframework.cache to
    find bugs. Removed logging.file from application-prod.properties.

--- Klucher/src/main/java/com/soze/Klucher.java
+++ Klucher/src/main/java/com/soze/Klucher.java
@@ -3,10 +3,12 @@ package com.soze;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class Klucher extends SpringBootServletInitializer {
   
   public static void main(String[] args) {
