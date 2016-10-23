495ab0da7e4186ab4b06075a10d5586c15cbfb43
sozemego <piknypiec@gmail.com>
2016-10-23 19:05:54 +0200

    Bug fix
    V5 of migration script - default first, then set to null.

--- Klucher/src/main/resources/db/migration/V5__klucher.sql
+++ Klucher/src/main/resources/db/migration/V5__klucher.sql
@@ -2,7 +2,7 @@ ALTER TABLE user
 ADD COLUMN profile_description varchar(140);
 
 ALTER TABLE user
-MODIFY kluchs_per_request int(11) NOT NULL DEFAULT 30;
+MODIFY kluchs_per_request int(11) DEFAULT 30 NOT NULL;
 
 ALTER TABLE user
 ADD COLUMN deleted bit(1) NOT NULL DEFAULT 0;
