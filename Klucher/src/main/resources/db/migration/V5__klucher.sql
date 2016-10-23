fcccefe4fabbbe8c750c31fecb51c05ef6e8cfa3
sozemego <piknypiec@gmail.com>
2016-10-23 22:20:12 +0200

    DB migration script
    Now properly sets already null values to default before modifying column
    as NOT NULL.

--- Klucher/src/main/resources/db/migration/V5__klucher.sql
+++ Klucher/src/main/resources/db/migration/V5__klucher.sql
@@ -1,8 +1,12 @@
 ALTER TABLE user
 ADD COLUMN profile_description varchar(140);
 
+UPDATE user
+SET kluchs_per_request = 30
+WHERE kluchs_per_request IS NULL;
+
 ALTER TABLE user
 MODIFY kluchs_per_request int(11) DEFAULT 30 NOT NULL;
 
 ALTER TABLE user
-ADD COLUMN deleted bit(1) NOT NULL DEFAULT 0;
+ADD COLUMN deleted bit(1) DEFAULT 0 NOT NULL;
